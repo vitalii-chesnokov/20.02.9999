3ac5107a4eb658d6b18caae2e514ad8a503e9b65
opilane <opilane@HP-CZC2349HTB>
2024-02-20 09:50:13 +0200

    Add project files.

---
+++ _20.02.24.py
@@ -0,0 +1,16 @@
+﻿from re import M
+from string import *
+from random import *
+
+#6
+list_=[]
+m=randint(1,30)
+for i in range(m):
+    list_.insert(i,randint(1,1000))
+print(list_)
+max_=max(list_)
+print("Maximum:",max_,",","Kokku",m)
+arv=max_//m
+indeks=list_.index(max_)
+list_[indeks]=arv
+print(list_)
